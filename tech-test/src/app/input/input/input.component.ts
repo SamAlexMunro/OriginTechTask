fcf0d6b509ff0794c429cd2a2d6ccae88e7123d6
Sam M <emberdesignltd@protonmail.com>
2021-08-10 21:12:40 +0100

    feat(Input): remove module

--- tech-test/src/app/input/input/input.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-
-@Component({
-  selector: "app-input",
-  templateUrl: "./input.component.html",
-  styleUrls: ["./input.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class InputComponent {}
