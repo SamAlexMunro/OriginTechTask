194ff77e954ba0419e5d6d4d5a5c82abb4839756
Sam M <emberdesignltd@protonmail.com>
2021-08-10 20:10:08 +0100

    feat(Card): Add card styling and HTML semantics

--- tech-test/src/app/card/card/card.component.ts
+++ tech-test/src/app/card/card/card.component.ts
@@ -1,5 +1,13 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 
+/**
+ * For this technical task due to the time constraint I've kept this component
+ * very simple, but ideally given more time I'd build it out to be a little more
+ * dynamic in terms of what can offer, with various attribute selectors such as;
+ * <app-card medium-spacing> ....
+ * <app-card> <app-card-header banner-header> ...
+ * ect
+ */
 @Component({
   selector: "app-card",
   templateUrl: "./card.component.html",
