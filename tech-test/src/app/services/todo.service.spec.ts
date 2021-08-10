fcaacb062f441c542da5d70c585267d7a885256c
Sam M <emberdesignltd@protonmail.com>
2021-08-10 19:46:50 +0100

    feat(TodoService): Add logic with relevant unit tests

--- tech-test/src/app/services/todo.service.spec.ts
+++ tech-test/src/app/services/todo.service.spec.ts
@@ -1,8 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { TodoService } from "./todo.service";
 
-import { TodoService } from './todo.service';
-
-describe('TodoService', () => {
+describe("TodoService", () => {
   let service: TodoService;
 
   beforeEach(() => {
@@ -10,7 +9,35 @@ describe('TodoService', () => {
     service = TestBed.inject(TodoService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  describe("$todoList should", () => {
+    it("initialised to an empty todo array ", () => {
+      expect(service.$todoList.value).toEqual([]);
+    });
+  });
+
+  describe("addNewTodo() should", () => {
+    it("Add a new todo item into the todoList$ obs with the correct properties", () => {
+      expect(service.$todoList.value).toEqual([]);
+      service.addNewTodo("Example");
+      const todoItem = service.$todoList.value[0];
+      expect(todoItem.description).toBe("Example");
+      expect(todoItem.edit).toBe(false);
+      expect(todoItem.done).toBe(false);
+    });
+  });
+
+  describe("removeTodo() should", () => {
+    it("Remove the correct todo item from the todoList$ obs", () => {
+      service.addNewTodo("Example");
+      service.addNewTodo("Example2");
+      expect(service.$todoList.value.length).toBe(2);
+      service.removeTodo(service.$todoList.value[1]);
+      expect(service.$todoList.value.length).toBe(1);
+      expect(service.$todoList.value[0].description).toBe("Example");
+    });
   });
 });
--- tech-test/src/app/services/todo.service.ts
+++ tech-test/src/app/services/todo.service.ts
@@ -1,6 +1,33 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+export interface Todo {
+  description: string;
+  done: boolean;
+  edit: boolean;
+  toggleEdit: Function;
+}
 
 @Injectable({ providedIn: "root" })
 export class TodoService {
-  constructor() {}
+  readonly $todoList = new BehaviorSubject<Todo[]>([]);
+
+  addNewTodo(description: string): void {
+    const todoClone = [...this.$todoList.value];
+    todoClone.push({
+      description,
+      done: false,
+      edit: false,
+      toggleEdit: function () {
+        this.edit = !this.edit;
+      },
+    });
+    this.$todoList.next([...todoClone]);
+  }
+
+  removeTodo(todo: Todo): void {
+    const todoClone = [...this.$todoList.value];
+    todoClone.splice(todoClone.indexOf(todo), 1);
+    this.$todoList.next([...todoClone]);
+  }
 }
