8cf489058ee797d06ef404109b1b8ff2f75e9728
Sam M <emberdesignltd@protonmail.com>
2021-08-10 20:51:47 +0100

    feat(App): Add default route and router-outlet to html

--- tech-test/src/app/app-routing.module.ts
+++ tech-test/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./todo/todo.module").then((m) => m.TodoModule),
+  },
+];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules,
-    relativeLinkResolution: 'legacy'
-}),
+      preloadingStrategy: PreloadAllModules,
+      relativeLinkResolution: "legacy",
+    }),
   ],
   exports: [RouterModule],
 })
