2c849908ccf7dafc1d5f7ac2f3a09a111dc79344
Sam M <emberdesignltd@protonmail.com>
2021-08-11 09:20:22 +0100

    feat(Button): Extend button component for cancel and submit buttons

--- tech-test/src/app/button/button/button.component.ts
+++ tech-test/src/app/button/button/button.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
-  selector: `button[origin-button], a[origin-anchor]`,
+  selector: `
+  button[origin-button],
+  button[origin-button-cancel],
+  button[origin-button-confirm],
+  a[origin-anchor]`,
   templateUrl: "./button.component.html",
   styleUrls: ["./button.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
