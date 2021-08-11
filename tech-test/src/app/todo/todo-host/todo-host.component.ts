8aed5eca128f2db9957486ebc78a509c308427c7
Sam M <emberdesignltd@protonmail.com>
2021-08-11 10:08:39 +0100

    Feat(TodoHost): Add todo item and todoList obs to html

--- tech-test/src/app/todo/todo-host/todo-host.component.ts
+++ tech-test/src/app/todo/todo-host/todo-host.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { TodoService } from "./../../services/todo.service";
 
 @Component({
   selector: "app-todo-host",
@@ -6,4 +7,7 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   styleUrls: ["./todo-host.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodoHostComponent {}
+export class TodoHostComponent {
+  readonly $todoList = this.todoService.$todoList;
+  constructor(private readonly todoService: TodoService) {}
+}
