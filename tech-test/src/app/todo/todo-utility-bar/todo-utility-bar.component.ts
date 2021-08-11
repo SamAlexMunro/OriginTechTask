09b0283afcf7b1f7c34ba8ce039742f6440a2d89
Sam M <emberdesignltd@protonmail.com>
2021-08-11 11:08:59 +0100

    feat(TodoUtility): Integrate search functionality

--- tech-test/src/app/todo/todo-utility-bar/todo-utility-bar.component.ts
+++ tech-test/src/app/todo/todo-utility-bar/todo-utility-bar.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from "@angular/core";
+import { fromEvent, ReplaySubject } from "rxjs";
+import { debounceTime, distinctUntilChanged, takeUntil } from "rxjs/operators";
+import { UserEvent } from "../../enums/user-events";
+import { TodoService } from "./../../services/todo.service";
 
 @Component({
   selector: "app-todo-utility-bar",
@@ -6,10 +16,30 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   styleUrls: ["./todo-utility-bar.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodoUtilityBarComponent {
+export class TodoUtilityBarComponent implements OnDestroy {
   addNewTodoPaneVisible = false;
+  @ViewChild("filter") filter: ElementRef<HTMLInputElement>;
+  private readonly destroy$ = new ReplaySubject<void>(1);
+
+  constructor(private readonly todoService: TodoService) {}
 
   toggleAddNewTodo(): void {
     this.addNewTodoPaneVisible = !this.addNewTodoPaneVisible;
+  }
+
+  ngAfterViewInit() {
+    this.setupFilter();
+  }
+
+  setupFilter(): void {
+    fromEvent(this.filter.nativeElement, UserEvent.KEYUP)
+      .pipe(debounceTime(150), distinctUntilChanged(), takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.todoService.filterList(this.filter.nativeElement.value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
   }
 }
